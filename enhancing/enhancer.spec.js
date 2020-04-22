bb4bfec1d0dbf4b41fd1b538cbdf5a1423997671
Justin Russell <Justin@Justins-MacBook.local>
2020-04-22 14:28:54 -0700

    enhancer.js functions done, 2 tests in spec done

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,23 @@
-const enhancer = require('./enhancer.js');
+const { succeed, fail, repair, get } = require('./enhancer.js');
 // test away!
+const item = {
+    name: "shield",
+    durability: 0,
+    enhancement: 0
+}
+
+describe("enhancer.js", () => {
+    it("should run tests without any errors", () => {
+        expect(true).toBe(true);
+    });
+
+    describe("repair()", () => {
+        it("should take an item and return that item with its durability at 100", () => {
+            repair(item);
+            expect(item.durability).toBe(100);
+        })
+    });
+
+    // describe("succeed")
+})
+
